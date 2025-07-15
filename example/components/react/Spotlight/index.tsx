5babb95a12c382642ad7f870478def887d12d3cb
he <476532333@qq.com>
2025-07-15 22:48:37 +0800

    docs(笔记): 更新水印组件实现与使用说明
    - 修改文档标题为《水印组件实现与使用》
    - 更新文档内容，介绍水印组件的实现原理和使用方法

---
+++ example/components/react/Spotlight/index.tsx
@@ -0,0 +1,18 @@
+import './index.scss';
+const Spotlight = () => {
+	return (
+		<div className="w-full bg-black p-8">
+			<div className="title mb-8" data-title="聚光灯效果1">
+				聚光灯效果1
+			</div>
+			<div className="title-two" data-title="聚光灯效果2">
+				聚光灯效果2
+			</div>
+
+			<div className="title-three" data-title="聚光灯效果3">
+				聚光灯效果3
+			</div>
+		</div>
+	);
+};
+export default Spotlight;
